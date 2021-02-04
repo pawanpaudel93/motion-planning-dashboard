776629de5b91b700efb90500f8968723c1df6465
Pawan Paudel <pawanpaudel93@gmail.com>
2021-02-04 18:15:01 +0545

    added postgres for dev environment

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -14,7 +14,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
