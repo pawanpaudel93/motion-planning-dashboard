8acebf382fe7bcadbc8b1d6bffdd1de009e55ce9
Pawan Paudel <pawanpaudel93@gmail.com>
2021-02-04 18:02:21 +0545

    created some database models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Session(models.Model):
+    target_altitude = models.IntegerField("Taeget Altitide")
+
+
+class BaseModel(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    value = ArrayField(models.FloatField())
+
+    class Meta:
+        abstract = True
+
+
+class Movement(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    value = ArrayField(models.IntegerField())
+
+
+class GlobalPosition(BaseModel):
+    class Meta:
+        verbose_name = _("Global Position")
+        verbose_name_plural = _("Global Positions")
+        db_table = "global_position"
+
+
+class GlobalHome(BaseModel):
+    class Meta:
+        verbose_name = _("Global Home")
+        verbose_name_plural = _("Global Home")
+        db_table = "global_home"
+
+
+class LocalPosition(BaseModel):
+    class Meta:
+        verbose_name = _("Local Position")
+        verbose_name_plural = _("Local Positions")
+        db_table = "local_position"
+
+
+class LocalVelocity(BaseModel):
+    class Meta:
+        verbose_name = _("Local Velocity")
+        verbose_name_plural = _("Local Velocities")
+        db_table = "local_velocity"
