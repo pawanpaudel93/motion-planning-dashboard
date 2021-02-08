2c3d1546d46302d0267751758111139ebf46934b
Pawan Paudel <pawanpaudel93@gmail.com>
2021-02-08 21:04:29 +0545

    displaying the default base graph

--- src/views/Session.vue
+++ src/views/Session.vue
@@ -1,17 +1,81 @@
 <template>
-
+  <div id='myDiv'></div>
 </template>
 <script>
   import {mapGetters} from 'vuex';
+  import Plotly from 'plotly.js-dist'
+  import axios from 'axios'
   export default {
     name: "Session",
+    data: () => ({
+      mapData: {}
+    }),
     computed: {
-      ...mapGetters({
-        mapData: "getMapData",
-      })
+
+    },
+    methods: {
+      setMapData() {
+        axios.get(this.$store.state.endpoints.mapData + `${this.$route.params.sessionId}/`)
+          .then(res => {
+            console.log(res.data)
+            this.mapData = res.data;
+            var data = [
+              {
+                z: this.mapData.grid,
+                colorscale: 'Viridis',
+                showscale: false,
+                type: 'heatmap'
+              }
+            ];
+            for (let i=0; i<this.mapData.edges.length; i++) {
+              let [p1, p2] = this.mapData.edges[i]
+              data.push({
+                x: [p1[1], p2[1]],
+                y: [p1[0], p2[0]],
+                mode: "lines",
+                line: {
+                  color: "#001FFF",
+                  shape: 'spline',
+                  width: 2.5
+                },
+                type: 'scatter'
+              })
+            }
+            var layout = {
+              title: 'Session Data',
+              width: 700,
+              height: 700,
+              showlegend: false,
+              xaxis: {
+                title: {
+                  text: 'EAST',
+                  font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                  }
+                },
+              },
+              yaxis: {
+                title: {
+                  text: 'NORTH',
+                  font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                  }
+                }
+              }
+            };
+            Plotly.newPlot('myDiv', data, layout);
+          })
+          .catch(err => {
+            console.log(err.message);
+          })
+      },
     },
     created() {
-      this.$store.dispatch("setMapData", this.$route.params.sessionId)
-    }
+      this.setMapData();
+    },
   }
 </script>
