32327dd99f2201eb5a15b446d171dd81b8c073f2
Pawan Paudel <pawanpaudel93@gmail.com>
2021-02-09 20:09:32 +0545

    added models to admin and open vue views if no route found on django

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,10 @@
-"""Project Name URL Configuration
+"""PX4-AUTOPILOT URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from .api import urls as api_urls
 from .api.views import index_view
@@ -14,5 +14,5 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('api/v1/', include(api_urls)),
     path('admin/', admin.site.urls),
-    path('', index_view, name='index'),
+    re_path(r'^.*$', index_view, name='index')
 ]
