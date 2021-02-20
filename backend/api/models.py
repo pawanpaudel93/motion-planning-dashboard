889d35ea0c322dbff8e4c020cd001801b5cc5a51
Pawan Paudel <pawanpaudel93@gmail.com>
2021-02-20 17:08:18 +0545

    restructured the database and showing coordinates in timeseries chart

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,56 +9,34 @@ class Session(models.Model):
     goal = ArrayField(models.FloatField(), null=True)
     is_finished = models.BooleanField("Is Finished", default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         verbose_name = _('Session')
         verbose_name_plural = _('Sessions')
         db_table = "session"
 
 
-class BaseModel(models.Model):
+class Movement(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True)
     value = ArrayField(models.FloatField())
 
-    class Meta:
-        abstract = True
-
-
-class Movement(BaseModel):
     class Meta:
         get_latest_by = 'id'
         verbose_name = _("Movement")
         verbose_name_plural = _("Movements")
         db_table = "movement"
 
+class DroneData(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    local_position = ArrayField(models.FloatField())
+    local_velocity = ArrayField(models.FloatField())
+    global_position = ArrayField(models.FloatField())
+    global_home = ArrayField(models.FloatField())
 
-class GlobalPosition(BaseModel):
     class Meta:
         get_latest_by = 'id'
-        verbose_name = _("Global Position")
-        verbose_name_plural = _("Global Positions")
-        db_table = "global_position"
-
-
-class GlobalHome(BaseModel):
-    class Meta:
-        get_latest_by = 'id'
-        verbose_name = _("Global Home")
-        verbose_name_plural = _("Global Home")
-        db_table = "global_home"
-
-
-class LocalPosition(BaseModel):
-    class Meta:
-        get_latest_by = 'id'
-        verbose_name = _("Local Position")
-        verbose_name_plural = _("Local Positions")
-        db_table = "local_position"
-
-
-class LocalVelocity(BaseModel):
-    class Meta:
-        get_latest_by = 'id'
-        verbose_name = _("Local Velocity")
-        verbose_name_plural = _("Local Velocities")
-        db_table = "local_velocity"
+        verbose_name = _("Drone Data")
+        verbose_name_plural = _("Drone Datas")
+        db_table = "drone_data"
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, re_path, include
 from .views import (
     SessionViewSet, SessionModelViewSet,
-    MovementViewSet, GlobalPositionViewSet,
-    GlobalHomeViewSet, LocalPositionViewSet, 
-    LocalVelocityViewSet, SimulationData,
-    SimulationTableData
+    MovementViewSet, DroneDataViewSet,
+    SimulationData, SimulationTableData
 )
 
 from rest_framework.routers import DefaultRouter
@@ -13,10 +11,7 @@ router = DefaultRouter()
 router.register(r'sessions', SessionModelViewSet, basename='sessions'),
 router.register(r'session', SessionViewSet, basename='session')
 router.register(r'movements', MovementViewSet, basename='movements')
-router.register(r'gpositions', GlobalPositionViewSet, basename='gpositions')
-router.register(r'ghomes', GlobalHomeViewSet, basename='ghomes')
-router.register(r'lpositions', LocalPositionViewSet, basename='lpositions')
-router.register(r'lvelocities', LocalVelocityViewSet, basename='lvelocities')
+router.register(r'drones-data', DroneDataViewSet, basename='drones-data')
 
 urlpatterns = [
     re_path(r'^', include(router.urls)),
